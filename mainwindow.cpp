5c52d5cc1ae3c0f8c73767f1b2e2d325ff9688cb
Junguilo <Tulcusss@gmail.com>
2024-03-30 23:22:03 -0700

    setting up

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,60 @@
 #include "mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-{}
+MainWindow::MainWindow(QWidget *parent) : QOpenGLWidget(parent) {}
 
 MainWindow::~MainWindow() {}
+
+void MainWindow::initializeGL(){
+    initializeOpenGLFunctions();
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+}
+
+void MainWindow::resizeGL(int w, int h){
+    glViewport(0,0,w,h);
+}
+
+void MainWindow::paintGL(){
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glBegin(GL_LINES);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex2f(-0.5f, 0.0f);
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex2f(0.5f, 0.0f);
+    glEnd();
+
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glBegin(GL_LINES);
+    glVertex2f(0.0f, -0.5f);
+    glVertex2f(0.0f, 0.5f);
+    glEnd();
+
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glBegin(GL_TRIANGLES);
+    glVertex2f(0.1f, 0.1f);
+    glVertex2f(0.4f, 0.1f);
+    glVertex2f(0.25f, 0.4f);
+    glEnd();
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *event) {
+    switch(event->key()) {
+    case Qt::Key_W:
+        // Handle 'w' key pressed
+        break;
+    case Qt::Key_A:
+        // Handle 'a' key pressed
+        break;
+    case Qt::Key_S:
+        // Handle 's' key pressed
+        break;
+    case Qt::Key_D:
+        // Handle 'd' key pressed
+        break;
+    case Qt::Key_Escape:
+        QApplication::exit();
+        break;
+    default:
+        QOpenGLWidget::keyPressEvent(event);
+    }
+}
--- mainwindow.h
+++ mainwindow.h
@@ -1,14 +1,24 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
+#include <QOpenGLWidget>
+#include <QApplication>
+#include <QOpenGLFunctions>
+#include <QDebug>
+#include <QKeyEvent>
 
-class MainWindow : public QMainWindow
+class MainWindow : public QOpenGLWidget, protected QOpenGLFunctions
 {
     Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+protected:
+    void initializeGL() override;
+    void resizeGL(int w, int h) override;
+    void paintGL() override;
+    void keyPressEvent(QKeyEvent *event) override;
 };
 #endif // MAINWINDOW_H
