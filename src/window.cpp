e66dd9e9f757ce5e86d25ce19da9b4625bdfe6ce
Junguilo <Tulcusss@gmail.com>
2024-04-04 16:27:16 -0700

    minor changes

--- src/window.cpp
+++ src/window.cpp
@@ -22,13 +22,16 @@ void Window::setupGLWidget(){
     //setup our text
     QLabel *test = new QLabel(tr("testin"));
     test->setAlignment(Qt::AlignHCenter);
+    QLabel *testTwo = new QLabel(tr("testinTwo"));
+    testTwo->setAlignment(Qt::AlignHCenter);
 
     //We don't use QMainWindow because that already sets up our layout for us
     //we want to use QGridLayout
     //Grid layout to have our window separate to buttons on the bottom
     QGridLayout *layout = new QGridLayout;
-    layout->addWidget(glWidget, 0,0);
+    layout->addWidget(glWidget, 0,0, 1, 2); //last two digits, rowspan, colspan
     layout->addWidget(test, 1, 0);
+    layout->addWidget(testTwo, 1, 1);
     setLayout(layout);
 
 }
